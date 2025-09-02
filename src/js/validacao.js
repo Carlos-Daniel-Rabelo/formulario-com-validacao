d9fdc07d8691c57e01b2bf43bded25705d067f8c
Carlos-Daniel-Rabelo <Carlos_Dan_Rabelo@hotmail.com>
2025-09-02 13:29:32 -0300

    tive que voltar ao trabalho 13:30

--- src/js/validacao.js
+++ src/js/validacao.js
@@ -1,3 +1,16 @@
-const formulario = document.getElementById("#formulario");
+const formulario = document.getElementById("formulario");
 const camposDeEntrada = document.querySelectorAll("input, textarea");
 
+formulario.addEventListener("submit", function(event) {
+    event.preventDefault(); // impede o envio automático
+})
+
+let invalido = false;
+
+camposDeEntrada.forEach(campo => {
+    if (campo.value.trim() === "") {
+        campo.classList.add(invalido);
+        
+        invalido = true;
+    }
+})
