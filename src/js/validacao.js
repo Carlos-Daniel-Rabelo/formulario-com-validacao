559996f0c1af71d619d13f83ee26363d995cd114
Carlos-Daniel-Rabelo <Carlos_Dan_Rabelo@hotmail.com>
2025-09-04 07:18:35 -0300

    não estou conseguindo validar
    estou travado na lógica de validação com javascript

--- src/js/validacao.js
+++ src/js/validacao.js
@@ -1,16 +1,7 @@
 const formulario = document.getElementById("formulario");
 const camposDeEntrada = document.querySelectorAll("input, textarea");
+const span = document.querySelectorAll("span");
 
-formulario.addEventListener("submit", function(event) {
-    event.preventDefault(); // impede o envio automático
-})
-
-let invalido = false;
-
-camposDeEntrada.forEach(campo => {
-    if (campo.value.trim() === "") {
-        campo.classList.add(invalido);
-        
-        invalido = true;
-    }
+formulario.addEventListener("submit", function (event) {
+    
 })
