0dc2559408a109070e3e138c8e34e0691496b364
Carlos-Daniel-Rabelo <Carlos_Dan_Rabelo@hotmail.com>
2025-09-04 15:07:05 -0300

    não estou conseguindo validar

--- src/js/validacao.js
+++ src/js/validacao.js
@@ -1,7 +1,8 @@
 const formulario = document.getElementById("formulario");
 const camposDeEntrada = document.querySelectorAll("input, textarea");
-const span = document.querySelectorAll("span");
+const spans = document.querySelectorAll("span");
 
 formulario.addEventListener("submit", function (event) {
-    
+    event.preventDefault();
+
 })
