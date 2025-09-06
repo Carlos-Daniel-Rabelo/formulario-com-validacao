ceb844b0c216f4a8190744ddeb7f6dba7d188718
Carlos-Daniel-Rabelo <Carlos_Dan_Rabelo@hotmail.com>
2025-09-06 09:48:55 -0300

    refatorando o html e javascript
    removir os atributo novalidate,  required e action

--- src/js/validacao.js
+++ src/js/validacao.js
@@ -1,8 +1,12 @@
 const formulario = document.getElementById("formulario");
 const camposDeEntrada = document.querySelectorAll("input, textarea");
-const spans = document.querySelectorAll("span");
 
 formulario.addEventListener("submit", function (event) {
     event.preventDefault();
-
-})
+    
+    camposDeEntrada.forEach(campo => {
+        if (campo.value.trim() === "") {
+            console.log("o campo ta vazio");
+        }
+    });
+});
