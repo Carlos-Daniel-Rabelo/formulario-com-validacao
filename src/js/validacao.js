7baf8e6505c74de1302def9a78870d06e7729742
Carlos-Daniel-Rabelo <Carlos_Dan_Rabelo@hotmail.com>
2025-09-09 07:34:45 -0300

    refatorando
    estou aplicando melhorias

--- src/js/validacao.js
+++ src/js/validacao.js
@@ -1,12 +1,6 @@
 const formulario = document.getElementById("formulario");
-const camposDeEntrada = document.querySelectorAll("input, textarea");
+const campos = document.querySelectorAll("input, textarea");
 
 formulario.addEventListener("submit", function (event) {
     event.preventDefault();
-    
-    camposDeEntrada.forEach(campo => {
-        if (campo.value.trim() === "") {
-            console.log("o campo ta vazio");
-        }
-    });
 });
