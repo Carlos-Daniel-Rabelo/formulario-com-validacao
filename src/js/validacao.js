5dc050e05d40dff9c783a0f26d70501386467896
Carlos-Daniel-Rabelo <Carlos_Dan_Rabelo@hotmail.com>
2025-09-10 07:09:49 -0300

    js quase pronto
    falta pouco

--- src/js/validacao.js
+++ src/js/validacao.js
@@ -1,6 +1,21 @@
-const formulario = document.getElementById("formulario");
-const campos = document.querySelectorAll("input, textarea");
+const formulario = document.querySelector(".formulario");
+const inputs = document.querySelectorAll(".input");
+const camposObrigatorio = document.querySelectorAll(".campo-obrigatorio");
 
-formulario.addEventListener("submit", function (event) {
+formulario.addEventListener('submit', (event) => {
     event.preventDefault();
+    // adicione sua lógica de tratamento de formulário aqui
+    inputs.forEach((input, index) => {
+        // obtive o valor de cada input individualmente
+        console.log(input.value)
+        console.log(index)
+
+        // verificar se os inputs estão com algum valor 
+        if (input.value.trim() === "") {
+            // input esta sem nenhum conteúdo
+            input.classList.add('invalido')
+            camposObrigatorio[index].classList.remove('campo-obrigatorio')
+        }
+        // para o else fazer o caminho contrario
+    })
 });
